87d774b37d34decf18bb092f17ee7dbfdc0afd14
Ibuki Ai <168512406+NevaGivAP@users.noreply.github.com>
2024-08-09 13:53:07 +0700

    Finish exercise 1 - 3
    I'm having trouble with the 4th exercise, I commit early so that others
    could check my code for the 4th exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...arguments) {
+    let converted = arguments;
+    let filteredArray= array.filter(function(item){
+        for(let i = 0; i < converted.length; i++){
+            if(item === converted[i]){
+                return false;
+            }
+        }
+        return true;
+    })
+    return filteredArray;
 };
-
+console.log(removeFromArray([1,2,3,4],3));
 // Do not edit below this line
 module.exports = removeFromArray;
